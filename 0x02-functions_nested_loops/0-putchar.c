437c2f5dfe4c5bcde2af8414f85a57b51ba55b8f
Akinyemi <afolabiakinyemi9@gmail.com>
2022-03-15 13:47:25 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,18 @@
-#include <main.h>
+#include "main.h"
 
+/**
+ * main -  prints School, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar:\n")
-	return (0);
+	_putchar('S');
+	_putchar('c');
+	_putchar('h');
+	_putchar('o');
+	_putchar('0');
+	_putchar('l');
+	_putchar('\n');
 
+	return (0);
 }
