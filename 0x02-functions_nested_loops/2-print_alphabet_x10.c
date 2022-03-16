759e22f6fc57894f44347fc192e580859028e258
Akinyemi <afolabiakinyemi9@gmail.com>
2022-03-16 16:56:52 +0100

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * print_alphabet - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
 void print_alphabet(void)
