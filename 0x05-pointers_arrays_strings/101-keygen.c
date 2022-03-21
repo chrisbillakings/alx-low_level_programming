f7ffba19c0480ebaae1f7fee9ebd6e21b894efbe
Akinyemi <afolabiakinyemi9@gmail.com>
2022-03-21 17:53:12 +0100

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+
+	int num = rand();
+	srand(time(0));
+	printf("%i\n",num);
+	return (0);
+}
