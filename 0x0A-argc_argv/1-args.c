9cd66a6c8ac5bf29996566183e8261aa676845c5
Akinyemi <afolabiakinyemi9@gmail.com>
2022-04-01 23:51:41 +0100

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,21 +1,19 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
 
-
 /**
- * main - Print all arguments
- * @argc: size of *argv
- * @argv: array of strings
- * Return: 0 on success
+ * main - prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i = 0;
+	int sum = 0;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-
+	(void)argv;
+	while (--argc)
+		sum++;
+	printf("%i\n", sum);
 	return (0);
 }
